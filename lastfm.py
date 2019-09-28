527723107df53b286ee70165adc8be3339317ea3
Graham Johnson <suspendedreason@gmail.com>
2019-09-28 14:40:31 -0400

    Pulling lastfm.py into functions, with a topArtists and topTracks func

--- lastfm.py
+++ lastfm.py
@@ -6,28 +6,42 @@ username = "gzuphoesdown"
 pause_duration = 0.5
 
 url = 'https://ws.audioscrobbler.com/2.0/?method=user.get{}&user={}&api_key={}&limit={}&extended={}&page={}&format=json'
-limit = 1 #api lets you retrieve up to 200 records per call
 extended = 0 #api lets you retrieve extended data for each track, 0=no, 1=yes
 page = 1 #page of results to start retrieving at
 
-method = 'toptracks'
-request_url = url.format(method, username, key, limit, extended, page)
-artist_names = []
-track_names = []
-play_counts = []
-response = requests.get(request_url).json()
 
-print(response)
-import pdb; pdb.set_trace()
-for item in response[method]['track']:
-    artist_names.append(item['artist']['name'])
-    track_names.append(item['name'])
-    play_counts.append(item['playcount'])
+def pullTopTracks(username, limit):
+    method = 'toptracks'
+    request_url = url.format(method, username, key, limit, extended, page)
+    artist_names = []
+    track_names = []
+    play_counts = []
+    response = requests.get(request_url).json()
 
-top_tracks = pd.DataFrame()
-top_tracks['artist'] = artist_names
-top_tracks['track'] = track_names
-top_tracks['play_count'] = play_counts
-top_tracks.to_csv('lastfm_project/lastfm_top_tracks.csv', index=None, encoding='utf-8')
-top_tracks.head()
+    print(response)
+    import pdb; pdb.set_trace()
+    for item in response[method]['track']:
+        artist_names.append(item['artist']['name'])
+        track_names.append(item['name'])
+        play_counts.append(item['playcount'])
 
+    # top_tracks = pd.DataFrame()
+    # top_tracks['artist'] = artist_names
+    # top_tracks['track'] = track_names
+    # top_tracks['play_count'] = play_counts
+    # top_tracks.to_csv('lastfm_project/lastfm_top_tracks.csv', index=None, encoding='utf-8')
+    # top_tracks.head()
+
+
+def pullTopArtists(username, limit):
+    method = 'topartists'
+    request_url = url.format(method, username, key, limit, extended, page)
+    response = requests.get(request_url).json()
+    # print(response)
+    artists = []
+    for item in response[method]['artist']:
+        artists.append(item['name'])
+
+    print(artists)
+
+pullTopArtists("gzuphoesdown", 1000)
